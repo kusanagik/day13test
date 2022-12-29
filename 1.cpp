1bf3f510b7d4827ea1e12a65a9941e58dcb21b44
kusanagik <KYO1373435918@163.com>
2022-12-29 14:50:29 +0800

    add 3 cout

--- 1.cpp
+++ 1.cpp
@@ -3,5 +3,8 @@ using namespace std;
 int main(int argc, const char *argv[])
 {
 	cout<<"hello world"<<endl;
+	cout<<"the king of fighters"<<endl;
+	cout<<"the king of fighters"<<endl;
+	cout<<"the king of fighters"<<endl;
 	return 0;
 }
